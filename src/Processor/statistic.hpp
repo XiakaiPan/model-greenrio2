22a40fa9a3b7e6e25f388c84f27e63d248f5834d
XiakaiPan <13212017962@163.com>
2022-11-21 21:09:54 +0800

    stat: statistic modification

--- src/Processor/statistic.hpp
+++ src/Processor/statistic.hpp
@@ -5,7 +5,7 @@ namespace Emulator {
   uint64_t n_insnDispatched = 0; // per cycle
 
   void stat() {
-    printf("Average Number of Insn Dispatched Per cycle: %lf\n", (double)Emulator::n_insnDispatched/(double)Emulator::Clock::CurTick());
+    // printf("Average Number of Insn Dispatched Per cycle: %lf\n", (double)Emulator::n_insnDispatched/(double)Emulator::Clock::CurTick());
     printf("total insn: %d, total cycle: %d, IPC: %f\n", Emulator::n_insn, Emulator::Clock::CurTick(), (double)Emulator::n_insn / (double)Emulator::Clock::CurTick());
 
   }
